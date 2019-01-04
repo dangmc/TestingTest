377f7dab4b3c8b7ff588cb0e6cb32bc8f0be7f58
Dang Mc <dangmc95@gmail.com>
2019-01-04 16:33:26 +0700

    Adding factorial function

---
+++ factorial.py
@@ -0,0 +1,7 @@
+def factorial(n):
+  if n == 0:
+    return 1
+  res = 1
+  for i in range(n):
+    res *= (i + 1)
+  return res
---
+++ test_factorial.py
@@ -0,0 +1,5 @@
+from factorial import factorial
+
+def test_factorial():
+   assert factorial(0) == 1
+   assert factorial(4) == 24
